6984a186a0d019c4b0527326ceed14e0793d30f8
Carolpandolfe <carolpandolfe@yahoo.com.br>
2021-09-13 13:46:00 -0300

    Redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
-function reducer(state = 0, action) {
-  switch (action.type) {
-    case 'INCREMENTAR':
-      return state + 1;
-    default:
-      return state;  
-  }
-}
-
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+import store from './store/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -22,10 +10,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
